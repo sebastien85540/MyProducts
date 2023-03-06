5b485cc035986ae70b7bd1126b6d4661e7d330ef
sebastien85540 <sebastien.reynaud123@orange.fr>
2023-03-06 09:25:56 +0100

    front end for udemy project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./products/products.component";
+import {AddProductsComponent} from "./products/add-products/add-products.component";
+import {ListProductsComponent} from "./products/list-products/list-products.component";
+import {UpdateProductComponent} from "./products/update-product/update-product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "produits", component: ProductsComponent, children: [
+      {path: "add", component: AddProductsComponent},
+      {path: "list", component: ListProductsComponent},
+      {path: "updateProduct/:id", component: UpdateProductComponent}
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsComponent } from './products/products.component';
+import {AddProductsComponent} from "./products/add-products/add-products.component";
+ import {ListProductsComponent} from "./products/list-products/list-products.component";
+ import {FormsModule} from "@angular/forms";
+import { UpdateProductComponent } from './products/update-product/update-product.component';
+ import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    AddProductsComponent,
+    ListProductsComponent,
+    UpdateProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
